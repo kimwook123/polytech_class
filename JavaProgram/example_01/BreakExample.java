eebaa1b802ba3f7f1478f5416df635386b779d3e
kimwook123 <rladnr1467@naver.com>
2026-03-31 17:20:09 +0900

    Add files via upload

---
+++ JavaProgram/example_01/BreakExample.java
@@ -0,0 +1,23 @@
+package example_01;
+
+import java.util.Scanner;
+
+public class BreakExample {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("exit를 입력하면 종료");
+		
+		while(true) {
+			System.out.println(">>");
+			String text = scanner.nextLine();
+			if(text.equals("exit"))
+				break;
+		}
+		System.out.println("종료");
+		
+		scanner.close();
+	}
+
+}
