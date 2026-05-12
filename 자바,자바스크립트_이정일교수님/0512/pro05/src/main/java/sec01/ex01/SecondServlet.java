2a325b44c196a299a643af39ca79c403274de05e
kimwook123 <rladnr1467@naver.com>
2026-05-12 17:29:49 +0900

    서블릿 학습

---
+++ 자바,자바스크립트_이정일교수님/0512/pro05/src/main/java/sec01/ex01/SecondServlet.java
@@ -0,0 +1,25 @@
+package sec01.ex01;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet{
+	@Override
+	public void init() throws ServletException{
+		System.out.println("init 메서드 호출>>>>");
+	}
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	throws ServletException, IOException{
+		System.out.println("doGet 메서드 호출>>>>");
+	}
+	@Override
+	public void destroy() {
+		System.out.println("destroy 메서드 호출>>>>");
+	}
+}
